7bc3bff1f00dbc950dc572355b0260e71cb8d693
Jakob Kruse <jakob.kruse@gmx.de>
2020-11-08 19:58:54 +0100

    Added impainting function to api service

--- src/app/core/service/api.service.ts
+++ src/app/core/service/api.service.ts
@@ -8,8 +8,7 @@ import { catchError, retry } from 'rxjs/operators';
 })
 export class ApiService {
   private readonly maskingURL = 'https://expingo-mask-service-oztyeiy7sa-uc.a.run.app/getMasks/';
-//  private readonly maskingURL = 'http://localhost:8080/getMasks/';
-  private readonly impaintingURL = '';
+  private readonly impaintingURL = 'https://expingo-inpaint-service-oztyeiy7sa-uc.a.run.app/inpaint/';
   constructor(private http: HttpClient) { }
   
   // Function that calls the masking API
@@ -22,6 +21,13 @@ export class ApiService {
     const formData: FormData = new FormData();
     formData.set('file', file);
     
-    return this.http.post<any>(this.maskingURL, formData, httpOptions)
+    return this.http.post<any>(this.maskingURL, formData, httpOptions);
+  }
+  
+  getImpaintedImage(image, mask) : Observable<any> {
+    const formData: FormData = new FormData();
+    formData.set('image', image);
+    formData.set('mask', mask);
+    return this.http.post<any>(this.impaintingURL, formData);
   }
 }
